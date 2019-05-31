2e8e65862edd9cc2c2a2f4c090d22dec6f27ed7b
Alex <zidz.ua@gmail.com>
2019-05-31 17:03:49 +0300

    upd

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const fn = compose([
-	x => x -8,
-	x => x ** 2,
-	(x,y) => (y > 0 ? x + 3 : x - 3)
+const fn = compose
+function compose(x,y){
+	let f = x => x - 8
+	let g = x => x ** 2
+	let k = (x,y) => (y > 0 ? x + 3 : x - 3)
 
-]);
+	return f(g(k(x,y)))
+}
 
 fn("3", 1);
 fn("3", -1);
