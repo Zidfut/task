05ad19b029d2d4a89988065e903cd1d6a584b406
Oleksandr Petryha <oleksandr.petryha@acropolium.com>
2019-06-13 16:26:03 +0300

    upd

--- calc.js
+++ calc.js
@@ -1,9 +1,9 @@
 function calc (firstNum, secondNum, operation, result) {
 	let operationResult;
 
-	let a = +parseFloat(firstNum).toFixed(2)
-	let b = +parseFloat(secondNum).toFixed(2)
-	let r = +parseFloat(result).toFixed(2)
+	let a = parseFloat(firstNum)
+	let b = parseFloat(secondNum)
+	let r = parseFloat(result)
 
 
 
@@ -13,43 +13,31 @@ function calc (firstNum, secondNum, operation, result) {
 		if(arr[i] >= 1 && arr[i] <= 1000000){
 			console.log(arr[i])
 		} else {
-			console.log('ошибка даных! ' + arr[i] + ' - число должно быть в диапазоне от 1 до 1000000!')
+			console.log(arr[i] + ' - число должно быть в диапазоне от 1 до 1000000!')
 			return 
 		}
 	}
 
-	console.log(r + " ожидаемый резултат")
-
-	function compare (x,z) {
-		
-		console.log(+z.toFixed(2) + " округленный результат операции")
-		if (x === +z.toFixed(2)) {
-			console.log("Результат операции совпадает с ожидаемым результатом!")
-		} else {
-			console.log("Результат операции  НЕ совпадает с ожидаемым результатом!")
-		}
+	function equal (x, y) {
+		return Math.abs(x - y) < Number.EPSILON * Math.max(Math.abs(x), Math.abs(y));
 	}
 
 	switch (operation) {
 		case "+":
 			operationResult = a + b;
-			console.log(operationResult + " результат операции")
-			compare(r, operationResult)
+			return equal(r, operationResult)
 			break;
 		case "-":
 			operationResult = a - b;
-			console.log(operationResult + " результат операции")
-			compare(r, operationResult)
+			return equal(r, operationResult)
 			break;
 		case "*":
 			operationResult = a * b;
-			console.log(operationResult + " результат операции")
-			compare(r, operationResult)
+			return equal(r, operationResult)
 			break;
 		case "/":
 			operationResult = a / b;
-			console.log(operationResult + " результат операции")
-			compare(r, operationResult)
+			return equal(r, operationResult)
 			break;
 		default:
 			console.log("Данного оператора не существует! Доступны +, -, *, /")
