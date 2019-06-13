0f51cdd86c18cb76a0c554c5f10ea4487fd35b43
Oleksandr Petryha <oleksandr.petryha@acropolium.com>
2019-06-13 10:32:09 +0300

    upd

--- calc.js
+++ calc.js
@@ -1,23 +1,27 @@
 function calc (firstNum, secondNum, operation, result) {
 	let operationResult;
 
-	let a = parseFloat(firstNum)
-	let b = parseFloat(secondNum)
-	let r = parseFloat(result)
+	let a = +parseFloat(firstNum).toFixed(2)
+	let b = +parseFloat(secondNum).toFixed(2)
+	let r = +parseFloat(result).toFixed(2)
+
+
 
 	let arr = [a,b]
 
 	for (let i = 0; i < arr.length; i++) {
 		if(arr[i] >= 1 && arr[i] <= 1000000){
-			
+			console.log(arr[i])
 		} else {
 			console.log('ошибка даных! ' + arr[i] + ' - число должно быть в диапазоне от 1 до 1000000!')
 			return 
 		}
 	}
 
+	console.log(r)
+
 	function compare (x,z) {
-		if (x == z) {
+		if (x === z) {
 			console.log("Результат операции совпадает с ожидаемым результатом!")
 		} else {
 			console.log("Результат операции  НЕ совпадает с ожидаемым результатом!")
@@ -27,6 +31,7 @@ function calc (firstNum, secondNum, operation, result) {
 	switch (operation) {
 		case "+":
 			operationResult = a + b;
+			console.log(operationResult)
 			compare(r, operationResult)
 			break;
 		case "-":
