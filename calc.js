10a9ad3de6439cf18069ae6968aefb5d4eee5ce5
Oleksandr Petryha <oleksandr.petryha@acropolium.com>
2019-06-13 16:39:54 +0300

    upd

--- calc.js
+++ calc.js
@@ -18,7 +18,7 @@ function calc (firstNum, secondNum, operation, result) {
 	}
 
 	function equal (x, y) {
-		return Math.abs(x - y) < Number.EPSILON * Math.max(Math.abs(x), Math.abs(y));
+		return Math.abs(x - y) < Number.EPSILON;
 	}
 
 	switch (operation) {
