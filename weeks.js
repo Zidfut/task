80ac82532d5eefa996b1bcd1fcdd90a21663a88b
Oleksandr Petryha <oleksandr.petryha@acropolium.com>
2019-06-21 16:49:29 +0300

    upd

--- weeks.js
+++ weeks.js
@@ -11,7 +11,10 @@ function groupWeeks(data) {
 		let dateOfMonth = dataDate.getDate()
 
 		if (dayOfWeek == 1) {
-			num > 1 ? acc.count = (acc.count / num).toFixed(2) : num = 1
+			if (num > 1) {
+				acc.count = (acc.count / num).toFixed(2)
+			}
+			num = 1
 			acc = {
 				weekStart: val.date,
 				count: val.count	
@@ -22,9 +25,7 @@ function groupWeeks(data) {
 			}
 		} else {
 			let accDate = new Date(acc.weekStart).getDate()
-
 			if ((accDate + 6) >= dateOfMonth) {
-
 				num = num + 1
 				acc.count = acc.count + val.count
 				if(dataLength == i + 1){
@@ -32,7 +33,10 @@ function groupWeeks(data) {
 				}
 			} else if (dayOfWeek == 0) {
 				let startWeek = new Date(dataDate.setDate(dateOfMonth - 6)).toLocaleDateString().split(".").reverse().join("-")
-				num > 1 ? acc.count = (acc.count / num).toFixed(2) : num = 1
+				if (num > 1) {
+					acc.count = (acc.count / num).toFixed(2)
+				}
+				num = 1
 				acc = {
 					weekStart: startWeek,
 					count: val.count
@@ -40,7 +44,10 @@ function groupWeeks(data) {
 				weeksArr.push(acc)
 			} else {
 				let startWeek = new Date(dataDate.setDate(dateOfMonth - (dayOfWeek - 1))).toLocaleDateString().split(".").reverse().join("-")
-				num > 1 ? acc.count = (acc.count / num).toFixed(2) : num = 1
+				if (num > 1) {
+					acc.count = (acc.count / num).toFixed(2)
+				}
+				num = 1
 				acc = {
 					weekStart: startWeek,
 					count: val.count		
